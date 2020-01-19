ffecf41c86e7660836653133021ffdcc5f8bbb86
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2020-01-18 21:26:40 -0500

    add a confirmation box in case the button is clicked accidentally

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,13 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     let url = new URL(tab.url);
-    let hostname = `http://gryphoncare.com/cgi-bin/luci/site_access_request/?url=${url.hostname}`;
-
-    $.post(hostname, function(_data, textStatus, xhr) {
-        if (xhr.status == 200) {
-            alert(`Submitted an unblock request for: ${url.hostname}\r\n\r\nNOTE: The site will remain blocked until the request is handled by your network admin, who can unblock it or leave it as-is.`)
-        } else {
-            alert(`A problem occurred while requesting an unblock for: ${url.hostname}\r\n\r\n${textStatus}`)
-        }
-    })
+    if (confirm(`Request that this site be unblocked? [${url.hostname}]`)) {
+        let postUrl = `http://gryphoncare.com/cgi-bin/luci/site_access_request/?url=${url.hostname}`;
+        $.post(postUrl, function(_data, textStatus, xhr) {
+            if (xhr.status == 200) {
+                alert(`Submitted an unblock request for: ${url.hostname}\r\n\r\nNOTE: The site will remain blocked until the request is handled by your network admin, who can unblock it or leave it as-is.`)
+            } else {
+                alert(`A problem occurred while requesting an unblock for: ${url.hostname}\r\n\r\n${textStatus}`)
+            }
+        })
+    }
 });
