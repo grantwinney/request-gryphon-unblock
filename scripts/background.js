a850ed42419db0dc6cd7b1fa18964d2194cdd4fe
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2020-01-20 11:12:57 -0500

    fix git merge weirdness

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     let url = new URL(tab.url);
-<<<<<<< HEAD
     let timeoutSeconds = 15;
     if (confirm(`Request that this site be unblocked? [${url.hostname}]\r\n\r\n(Note that after you press OK, it may take a few seconds to complete submitting the request before you see another prompt.)`)) {
         $.ajax({
@@ -20,15 +19,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
                         alert(`Unable to request an unblock for: ${url.hostname}\r\n\r\nReason: ${errorThrown}`);
                     }
                 }
-=======
-    if (confirm(`You're about to send a request to unblock ${url.hostname}.\r\n\r\nCONTINUE?`)) {
-        let postUrl = `http://gryphoncare.com/cgi-bin/luci/site_access_request/?url=${url.hostname}`;
-        $.post(postUrl, function(_data, textStatus, xhr) {
-            if (xhr.status == 200) {
-                alert(`Submitted a request to unblock ${url.hostname}.\r\n\r\nNOTE: The site will remain blocked until your network admin sees your request and unblocks it.`)
-            } else {
-                alert(`A problem occurred while requesting that ${url.hostname} be unblocked.\r\n\r\nSTATUS: ${textStatus}`)
->>>>>>> 48fdc33355b5ff200501bda492057bddf0201ac7
             }
         });
     }
